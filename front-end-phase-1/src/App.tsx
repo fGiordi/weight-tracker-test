b6c75ce3d8ebc1b9898a04003f571887b8258645
SolinkfGiordi <giordifungula@gmail.com>
2024-11-27 21:29:06 +0200

    - updated the app to point to the login page
    - made login page entry route

--- front-end-phase-1/src/App.tsx
+++ front-end-phase-1/src/App.tsx
@@ -1,12 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css'
+import { Login } from './pages/login';
 
 function App() {
 
   return (
     <BrowserRouter>
      <Routes>
-      <Route path="/" element={<h1>Home Page</h1>}></Route>
+      <Route path="/" element={<Login/>}></Route>
       {/* TODO to implement login route and dashboard/landing page */}
      </Routes>
     </BrowserRouter>
