080f332b8955431b7c1d5876acb4dacfff23965a
SolinkfGiordi <giordifungula@gmail.com>
2024-11-27 22:34:45 +0200

    updated auth store

--- front-end-phase-1/src/store/useAuthStore.ts
+++ front-end-phase-1/src/store/useAuthStore.ts
@@ -1,13 +1,30 @@
+import { toast } from '@/hooks/use-toast';
 import { create } from 'zustand';
 
 interface AuthState {
   isAuthenticated: boolean;
-  login?: (email: string, password: string) => Promise<boolean>;
+  login: (email: string, password: string) => Promise<boolean>;
   logout: () => void;
 }
 
 export const useAuthStore = create<AuthState>((set) => ({
   isAuthenticated: false,
-//   TODO to implement login logic
+  login: async (email: string, password: string) => {
+    try {
+//    TODO update this with backend logic later
+    if (
+        email === 'test@example.com' && 
+        password === 'password123'
+    ) {
+        set({ isAuthenticated: true });
+    }
+    return true;
+
+    } catch (error) {
+    console.error('Login failed:', error);
+    toast({ title: "Error not authorized", variant: 'destructive' });
+    return false;
+    }
+  },
   logout: () => set({ isAuthenticated: false }),
 }));
