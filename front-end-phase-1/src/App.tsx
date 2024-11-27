a6148d82d6e2e0fd29c5160bbab44373f3a0da61
SolinkfGiordi <giordifungula@gmail.com>
2024-11-27 20:38:28 +0200

    add shad ui button and base packages required
    - installed react-router-dom, zod and react-hook-form.
    - radix added when setup of the shadcn button component

--- front-end-phase-1/src/App.tsx
+++ front-end-phase-1/src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { Button } from "@/components/ui/button"
 import './App.css'
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
       <div>
+      <Button>Click me</Button>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
