068b909d7c28db6a6ebec7a6fc5b3b9be71097ee
SolinkfGiordi <giordifungula@gmail.com>
2024-11-27 22:33:47 +0200

    update App.tsx with Protected Rotue
    - Added Main Page which is protected.

--- front-end-phase-1/src/App.tsx
+++ front-end-phase-1/src/App.tsx
@@ -1,6 +1,14 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css'
 import { Login } from './pages/login';
+import { Main } from './pages/main'
+import { Toaster } from "@/components/ui/toaster"
+import { useAuthStore } from './store/useAuthStore';
+
+function ProtectedRoute({ children }: { children: React.ReactNode }) {
+  const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
+  return isAuthenticated ? <>{children}</> : <Navigate to="/" />;
+}
 
 function App() {
 
@@ -8,8 +16,16 @@ function App() {
     <BrowserRouter>
      <Routes>
       <Route path="/" element={<Login/>}></Route>
-      {/* TODO to implement login route and dashboard/landing page */}
+      <Route
+          path="/main"
+          element={
+            <ProtectedRoute>
+              <Main />
+            </ProtectedRoute>
+          }
+        />
      </Routes>
+     <Toaster />
     </BrowserRouter>
   )
 }
